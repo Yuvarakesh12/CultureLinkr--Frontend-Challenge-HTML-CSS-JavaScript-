6886caaf2c82d2744aa52dc90a494ca2e7fd54ee
yuvarakesh16 <yuvakadiyam@gmail.com>
2024-10-24 06:29:27 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+document.getElementById('contactForm').addEventListener('submit', function(event) {
+    event.preventDefault(); 
+
+    
+    document.getElementById('successMessage').classList.remove('hidden');
+
+    
+    this.reset();
+});
